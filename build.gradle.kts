fc4ce62ef9420d3ba5034d966d018bf4479d64e7
yug-yongsu <yozx234@gmail.com>
2022-05-08 01:54:37 +0900

    add vaadin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.5.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("com.vaadin") version "0.14.6.0"
     kotlin("jvm") version "1.6.20"
     kotlin("plugin.spring") version "1.6.20"
 }
@@ -17,11 +18,14 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2021.0.2"
+extra["vaadinVersion"] = "23.0.7"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+
+    implementation("com.vaadin:vaadin-spring-boot-starter")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
@@ -31,6 +35,12 @@ dependencies {
 
     implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.213"))
     implementation("com.amazonaws:aws-java-sdk-kinesis")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
