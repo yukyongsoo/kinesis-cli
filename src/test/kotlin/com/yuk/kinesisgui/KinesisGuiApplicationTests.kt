b438992b82f5cdd3116fe80575ee731e15295fec
yukyongsoo <yozx234@gmail.com>
2023-04-12 15:04:07 +0900

    fix(test): error by remove spring shell

--- src/test/kotlin/com/yuk/kinesisgui/KinesisGuiApplicationTests.kt
+++ src/test/kotlin/com/yuk/kinesisgui/KinesisGuiApplicationTests.kt
@@ -5,15 +5,8 @@ import com.yuk.kinesisgui.stream.KinesisService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.shell.jline.InteractiveShellApplicationRunner
-import org.springframework.shell.jline.ScriptShellApplicationRunner
 
-@SpringBootTest(
-    properties = [
-        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
-        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
-    ]
-)
+@SpringBootTest
 class KinesisGuiApplicationTests {
     @Autowired
     private lateinit var kinesisService: KinesisService
