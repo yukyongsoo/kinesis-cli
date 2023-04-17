d382c3a555b2b531c49144385dc6bd0ae7ed1120
yukyongsoo <yozx234@gmail.com>
2023-04-17 16:03:05 +0900

    feat(gui): add stream filter

--- src/main/kotlin/com/yuk/kinesisgui/gui/stream/StreamTabs.kt
+++ src/main/kotlin/com/yuk/kinesisgui/gui/stream/StreamTabs.kt
@@ -1,18 +1,20 @@
-package com.yuk.kinesisgui.gui
+package com.yuk.kinesisgui.gui.stream
 
 import com.vaadin.flow.component.tabs.Tab
 import com.vaadin.flow.component.tabs.Tabs
 import com.vaadin.flow.spring.annotation.SpringComponent
 import com.vaadin.flow.spring.annotation.UIScope
+import com.yuk.kinesisgui.gui.MainView
+import com.yuk.kinesisgui.gui.SessionContext
 
 @UIScope
 @SpringComponent
-class StreamListView(
+class StreamTabs(
     private val sessionContext: SessionContext,
     private val mainView: MainView
 ) : Tabs() {
     init {
-        orientation = Orientation.VERTICAL
+        orientation = Tabs.Orientation.VERTICAL
 
         val list = sessionContext.getStreamList()
         list.sorted().forEach {
@@ -25,5 +27,18 @@ class StreamListView(
             mainView.clear()
             sessionContext.changeCurrentStreamName(event.selectedTab.label)
         }
+    }
+
+    fun filterStream(value: String) {
+        val hideTarget = this.children.filter {
+            it is Tab && it.label.contains(value).not()
+        }
+
+        val showTarget = this.children.filter {
+            it is Tab && it.label.contains(value)
+        }
+
+        hideTarget.forEach { it.isVisible = false }
+        showTarget.forEach { it.isVisible = true }
     }
 }
